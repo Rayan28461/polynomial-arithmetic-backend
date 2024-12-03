8097cc7e97e714d92bb73e95fc8be4bcb42512fa
Karl-67 <karlgerges67@gmail.com>
2024-12-03 22:56:42 +0200

    updated mod

--- src/core/services/mod_reduction.py
+++ src/core/services/mod_reduction.py
@@ -25,19 +25,19 @@ def modReduction(poly1:str, divisor:str, inputType:str, m:int=163):
     irreduciblePoly=gf.irreducible_poly #Extract the irreducible polynomial stored in the library
     integerRep=int(irreduciblePoly) #Integer representation of the irreducible polynomial
 
-    #If the input is not compatible in the galois field of the input    
-    if inputType=='binary':
-        if int(poly1,2)>2**m:
-            poly1=gf2Mod(poly1, integerRep)
-        elif int(poly2,2)>2**m:
-            poly2=gf2Mod(poly2,integerRep)
-    elif inputType=='hexadecimal':
-        if int(poly1,16)>2**m:
-            poly1=gf2Mod(poly1, integerRep)
-        elif int(poly2,16)>2**m:
-            poly2=gf2Mod(poly2,integerRep)
-
     try:
+    #If the input is not compatible in the galois field of the input    
+        if inputType=='binary':
+            if int(poly1,2)>2**m:
+                poly1=gf2Mod(poly1, integerRep)
+            elif int(poly2,2)>2**m:
+                poly2=gf2Mod(poly2,integerRep)
+        elif inputType=='hexadecimal':
+            if int(poly1,16)>2**m:
+                poly1=gf2Mod(poly1, integerRep)
+            elif int(poly2,16)>2**m:
+                poly2=gf2Mod(poly2,integerRep)
+
         #Convert based on the input type to base 10
         if inputType=='binary': #If the input is in base 2
             fieldPoly1=(int(poly1,2))
@@ -48,27 +48,24 @@ def modReduction(poly1:str, divisor:str, inputType:str, m:int=163):
         else: #If the input is not in binary or decimal
             raise ValueError('Invalid input type!')
         
-        result=gf2Mod(fieldPoly1, fieldPoly2) #Compute the Remainder of the division (poly1%poly2)
+        result=gf2Mod(fieldPoly1, fieldPoly2, inputType, m) #Compute the Remainder of the division (poly1%poly2)
 
-        if inputType == 'binary': #Convert result into binary
-            return bin(int(result))[2:].zfill(m)  #Binary string padded to m bits and removing the prefix 
-        elif inputType == 'hexadecimal': #Convert result into hexadecimal
-            return hex(int(result))[2:].upper().zfill(m // 4)  #Hex string padded to m/4 characters and removing the prefix
-        return result #return the result 
+        return result
 
     except ValueError as e:
         raise ValueError(e)
         
 
-def gf2Mod(poly:int, mod:int):
+def gf2Mod(poly:int, mod:int, inputType:str, m:int):
     """
     Perform modulo reduction of a polynomial over GF(2^m).
     
     Arguments:
     poly: Integer representation of the polynomial.
     mod: Integer representation of the divisor polynomial.
+    inputType: The original format of the input specified by the user.
 
-    Returns: Integer representation of the remainder polynomial.
+    Returns: Either a binary or hexadecimal representation of the remainder polynomial.
     """
 
     mod_degree=mod.bit_length()-1  #Degree of the modulus polynomial
@@ -77,4 +74,8 @@ def gf2Mod(poly:int, mod:int):
         degree_diff=poly.bit_length() - mod_degree - 1 #Degree difference
         poly^=mod<<degree_diff #XOR the mod shifted to align with poly's leading term
     
-    return poly
+    if inputType == 'binary': #Convert result into binary
+            return bin(int(poly))[2:].zfill(m)  #Binary string padded to m bits and removing the prefix 
+    elif inputType == 'hexadecimal': #Convert result into hexadecimal
+            return hex(int(poly))[2:].upper().zfill(m // 4)  #Hex string padded to m/4 characters and removing the prefix
+    return poly #return the result 
